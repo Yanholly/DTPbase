0726603207c04d13ac14a5d92ea579cd6c6cde81
Max <m9khamets@gmail.com>
2019-06-11 17:14:10 +0300

    v 1.2

---
+++ Base_scheme/postgres_public_dtptypes.sql
@@ -0,0 +1,8 @@
+UPDATE public.dtptypes SET type_name = 'Столкновение' WHERE type_id = 1;
+UPDATE public.dtptypes SET type_name = 'Наезд на пешехода' WHERE type_id = 2;
+UPDATE public.dtptypes SET type_name = 'Наезд на велосипедиста' WHERE type_id = 3;
+UPDATE public.dtptypes SET type_name = 'Наезд на стоящее ТС' WHERE type_id = 4;
+UPDATE public.dtptypes SET type_name = 'Падение пассажира ' WHERE type_id = 5;
+UPDATE public.dtptypes SET type_name = 'Съезд с дороги' WHERE type_id = 6;
+UPDATE public.dtptypes SET type_name = 'Наезд на препятствие' WHERE type_id = 7;
+UPDATE public.dtptypes SET type_name = 'Иной вид ДТП' WHERE type_id = 8;
